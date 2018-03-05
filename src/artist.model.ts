c871c538ae590453d08efc51586faf75c903fa23
sam gespass <darth.toad@gmail.com>
2018-03-05 11:35:08 -0800

    create models

---
+++ src/artist.model.ts
@@ -0,0 +1,3 @@
+export class Artist {
+  constructor(public name: string, public location: string, public instruments: string[], public genres: string[], public bio: string, public lookingFor: string, public available: string, public currentGroups: string[], public formerGroups: string[]) {}
+}
---
+++ src/group.model.ts
@@ -0,0 +1,3 @@
+export class Artist {
+  constructor(public name: string, public location: string, public genre: string, public members: string[], public bio: string, public lookingFor: string[], public available: string) {}
+}
