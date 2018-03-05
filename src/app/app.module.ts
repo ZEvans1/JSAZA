610e5d2e9efbb59ee440554d28e2a1e82d7d253b
Aaron Ross and James Donlan <james@>
2018-03-05 10:08:31 -0800

    add beginning components and artist model

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/artist.model.ts
@@ -0,0 +1,2 @@
+export class Artist.Model {
+}
