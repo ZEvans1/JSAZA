18d87ab53376270692cd5429a23ac68354b887fb
sam gespass <darth.toad@gmail.com>
2018-03-05 13:39:00 -0800

    update JSON examples, change models

--- src/app/group.model.ts
+++ src/app/group.model.ts
@@ -1,3 +1,6 @@
-export class Artist {
-  constructor(public name: string, public location: string, public genre: string, public members: string[], public bio: string, public lookingFor: string[], public available: string) {}
+export class Group {
+  profileImage: string = "";
+  profileGallery: string[] = [""];
+  
+  constructor(public name: string, public location: string, public genre: string, public members: string[], public bio: string, public lookingFor: string[], public available: boolean) {}
 }
