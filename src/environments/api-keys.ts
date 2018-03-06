03445ea7e7131bb50050909f9d647f7f7850fd4b
Aaron Ross and Zach Evans <aaron+zach@>
2018-03-06 08:29:50 -0800

    update gitignore

--- src/environments/api-keys.ts
+++
@@ -1,8 +0,0 @@
-export var masterFirebaseConfig = {
-  apiKey: "AIzaSyCmq4xv-qJ6xz3QyaLLSFwpDMm_ICazPjg",
-  authDomain: "jsaza-2018.firebaseapp.com",
-  databaseURL: "https://jsaza-2018.firebaseio.com",
-  projectId: "jsaza-2018",
-  storageBucket: "jsaza-2018.appspot.com",
-  messagingSenderId: "589217810809"
-};
