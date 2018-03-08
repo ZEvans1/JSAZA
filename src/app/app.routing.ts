673f955b723f30b0a8865cc8213517e303d49e9d
Guest User <Guest@Epicodus-3D.local>
2018-03-08 14:47:35 -0800

    make form styling consistent

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -10,7 +10,6 @@ import { GroupSearchComponent } from './group-search/group-search.component'
 import { EditComponent } from './edit/edit.component';
 import { GroupEditComponent } from './group-edit/group-edit.component';
 import { GroupDashboardComponent } from './group-dashboard/group-dashboard.component';
-import { WelcomeComponent } from './welcome/welcome.component';
 
 const appRoutes: Routes = [
   {
@@ -60,10 +59,6 @@ const appRoutes: Routes = [
   {
     path:'view-group/:id',
     component: GroupDashboardComponent
-  },
-  {
-    path:'welcome',
-    component: WelcomeComponent
   }
 
 ];
--- src/app/create-artist/create-artist.component.ts
+++ src/app/create-artist/create-artist.component.ts
@@ -43,46 +43,45 @@ export class CreateArtistComponent implements OnInit {
       endDate: newGroupEndDate,
       verified: false
     }];
-    let that = this;
     let newArtist = new Artist(newName, location, this.instruments, this.genres, newTagline, newBio, this.allLookingFor, newAvailable, groups);
     newArtist.profileImage = newProfileImg;
     newArtist.gallery = [newGalleryURL1, newGalleryURL2, newGalleryURL3];
-    setTimeout(function() {
-      that.artistService.addArtist(newArtist, that.authService.userDetails.uid);
-      that.router.navigate(['dashboard/' + this.authService.userDetails.uid]);
-      }, 2000);
-    ;
-
+    setTimeout(() => {
+      this.artistService.addArtist(newArtist, this.authService.userDetails.uid);
+    }, 2000);
+    setTimeout(() => {
+      this.router.navigate(['/dashboard/' + this.authService.userDetails.uid]);
+    }, 2500);
   }
 
-  instrumentOptions = [
-    {value: "acoustic-guitar", name:"acoustic-guitar"},
-    {value: "alto-sax", name:"alto-sax"},
-    {value: "banjo", name:"banjo"},
-    {value: "bari-sax", name:"bari-sax"},
-    {value: "bass", name:"bass"},
-    {value: "bongos", name:"bongos"},
-    {value: "cello", name:"cello"},
-    {value: "jd", name:"jd"},
-    {value: "double-bass", name:"double-bass"},
-    {value: "drums", name:"drums"},
-    {value: "electric-bass", name:"electric-bass"},
-    {value: "electric-guitar", name:"electric-guitar"},
-    {value: "guitar", name:"guitar"},
-    {value: "keyboard", name:"keyboard"},
-    {value: "mastering", name:"mastering"},
-    {value: "mixing", name:"mixing"},
-    {value: "piano", name:"piano"},
-    {value: "post-production", name:"post-production"},
-    {value: "production", name:"production"},
-    {value: "soprano-sax", name:"soprano-sax"},
-    {value: "sound-engineer", name:"sound-engineer"},
-    {value: "synthesizer", name:"synthesizer"},
-    {value: "tenor-sax", name:"tenor-sax"},
-    {value: "trombone", name:"trombone"},
-    {value: "viola", name:"viola"},
-    {value: "violin", name:"violin"},
-    {value: "vocals", name:"vocals"}
-  ];
+  // instrumentOptions = [
+  //   {value: "acoustic-guitar", name:"acoustic-guitar"},
+  //   {value: "alto-sax", name:"alto-sax"},
+  //   {value: "banjo", name:"banjo"},
+  //   {value: "bari-sax", name:"bari-sax"},
+  //   {value: "bass", name:"bass"},
+  //   {value: "bongos", name:"bongos"},
+  //   {value: "cello", name:"cello"},
+  //   {value: "dj", name:"dj"},
+  //   {value: "double-bass", name:"double-bass"},
+  //   {value: "drums", name:"drums"},
+  //   {value: "electric-bass", name:"electric-bass"},
+  //   {value: "electric-guitar", name:"electric-guitar"},
+  //   {value: "guitar", name:"guitar"},
+  //   {value: "keyboard", name:"keyboard"},
+  //   {value: "mastering", name:"mastering"},
+  //   {value: "mixing", name:"mixing"},
+  //   {value: "piano", name:"piano"},
+  //   {value: "post-production", name:"post-production"},
+  //   {value: "production", name:"production"},
+  //   {value: "soprano-sax", name:"soprano-sax"},
+  //   {value: "sound-engineer", name:"sound-engineer"},
+  //   {value: "synthesizer", name:"synthesizer"},
+  //   {value: "tenor-sax", name:"tenor-sax"},
+  //   {value: "trombone", name:"trombone"},
+  //   {value: "viola", name:"viola"},
+  //   {value: "violin", name:"violin"},
+  //   {value: "vocals", name:"vocals"}
+  // ];
 
 }
