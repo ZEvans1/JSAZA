8b88c00806aa0263c9012d17963516b6b24afc0e
Aaron Ross <ashmortar@gmail.com>
2018-03-07 20:48:19 -0800

    update styling and make artist form more consistent to style theme

--- src/app/create-artist/create-artist.component.ts
+++ src/app/create-artist/create-artist.component.ts
@@ -4,6 +4,7 @@ import { ArtistService} from './../services/artist.service';
 import { Artist } from './../artist.model';
 import { Router } from '@angular/router';
 import {MaterializeDirective} from "angular2-materialize";
+import { Observable } from "rxjs/Rx";
 
 @Component({
   selector: 'app-create-artist',
@@ -25,6 +26,7 @@ export class CreateArtistComponent implements OnInit {
   constructor(private router: Router, private authService: AuthService, private artistService: ArtistService) { }
 
   ngOnInit() {
+
   }
 
   submitForm(newName: string, newEmail: string, newPassword: string, newCity: string, newState: string, newZip: string, newTagline: string, newBio: string, newAvailable: boolean, newGroupId: string, newGroupRole: string, newGroupStartDate: string, newGroupEndDate: string, newProfileImg: string, newGalleryURL1: string, newGalleryURL2: string, newGalleryURL3: string) {
@@ -56,5 +58,35 @@ export class CreateArtistComponent implements OnInit {
     let that = this;
     setTimeout(function() {console.log(that.authService.userDetails.uid)}, 1000);
   }
+
+  instrumentOptions = [
+    {value: "acoustic-guitar", name:"acoustic-guitar"},
+    {value: "alto-sax", name:"alto-sax"},
+    {value: "banjo", name:"banjo"},
+    {value: "bari-sax", name:"bari-sax"},
+    {value: "bass", name:"bass"},
+    {value: "bongos", name:"bongos"},
+    {value: "cello", name:"cello"},
+    {value: "jd", name:"jd"},
+    {value: "double-bass", name:"double-bass"},
+    {value: "drums", name:"drums"},
+    {value: "electric-bass", name:"electric-bass"},
+    {value: "electric-guitar", name:"electric-guitar"},
+    {value: "guitar", name:"guitar"},
+    {value: "keyboard", name:"keyboard"},
+    {value: "mastering", name:"mastering"},
+    {value: "mixing", name:"mixing"},
+    {value: "piano", name:"piano"},
+    {value: "post-production", name:"post-production"},
+    {value: "production", name:"production"},
+    {value: "soprano-sax", name:"soprano-sax"},
+    {value: "sound-engineer", name:"sound-engineer"},
+    {value: "synthesizer", name:"synthesizer"},
+    {value: "tenor-sax", name:"tenor-sax"},
+    {value: "trombone", name:"trombone"},
+    {value: "viola", name:"viola"},
+    {value: "violin", name:"violin"},
+    {value: "vocals", name:"vocals"}
+  ];
 
 }
